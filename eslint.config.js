aea143e8f8220a2c13e84d105346253aa4defa42
Mortis0114 <46196322+M0rt1s0114@users.noreply.github.com>
2026-08-20 01:23:27 +0800

    style: ignore generated plugin dist in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import globals from 'globals'
 
 export default tseslint.config(
   {
-    ignores: ['dist/**', 'node_modules/**', 'src-tauri/target/**', 'src-tauri/gen/**'],
+    ignores: ['**/dist/**', 'node_modules/**', 'src-tauri/target/**', 'src-tauri/gen/**'],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
